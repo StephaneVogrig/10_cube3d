deb4e4a89fa22c0358889a94bbe08f03da2535d1
Stephane Vogrig <svogrig@1G7.42angouleme.fr>
2024-09-11 17:11:17 +0200

    refactor entry_arg_checker

--- src/utils/check_utils.c
+++ src/utils/check_utils.c
@@ -6,7 +6,7 @@
 /*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 04:12:46 by aska              #+#    #+#             */
-/*   Updated: 2024/09/11 14:38:02 by svogrig          ###   ########.fr       */
+/*   Updated: 2024/09/11 17:09:57 by svogrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,13 @@
 
 void entry_arg_checker(int ac, char **av)
 {
-    int err;
     int strnstr;
 
-    err = 0;
     strnstr = 0;
-    err = chk_box(ac, EQ, 2, "Number of entry arguments");
-    goto_exit(err, err, ERROR, "No Parameter");
+    if (chk_box(ac, EQ, 2, "Number of entry arguments") != SUCCESS)
+        ft_exit(ERROR,ERROR, "Invalid number of parameters");
     if ((ft_strnstr(av[1], ".cub", ft_strlen(av[1]))) == NULL)
         strnstr = 1;
-    err = chk_box(strnstr, EQ, 0, "Check file extension");
-    goto_exit(err, err, ERROR, EXTENSION_ERROR);
+    if (chk_box(strnstr, EQ, 0, "File extension") != SUCCESS)
+        ft_exit(ERROR,ERROR, "No \".cub\" extension map file");
 }
