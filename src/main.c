0415dfc487132f7a5a51fb6d181b3196a1e5397a
Stephane Vogrig <svogrig@1A5.42angouleme.fr>
2024-09-15 17:12:14 +0200

    clean main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 02:16:47 by aska              #+#    #+#             */
-/*   Updated: 2024/09/14 15:17:18 by svogrig          ###   ########.fr       */
+/*   Updated: 2024/09/15 17:00:55 by svogrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,36 @@
 #include "render.h"
 #include "mlx.h"
 
-int	map_load(char *path, char **map)
+int	map_load(char *path, t_map *map)
 {
-	(void)path;
-	(void)map;
-	
 	printf("map_load\n");
+	(void)path;
+	
+	map->grid = malloc(9 * sizeof(map));
+	if (map == NULL)
+		return (FAIL);
+    map->grid[0] = "1111111111111111";
+    map->grid[1] = "1000000000000001";
+    map->grid[2] = "1001000000000111";
+    map->grid[3] = "1000000000000001";
+    map->grid[4] = "1000000000000001";
+    map->grid[5] = "1000000010000001";
+    map->grid[6] = "1000000000010001";
+    map->grid[7] = "1000100000000001";
+    map->grid[8] = "1111111111111111";
+	map->width = 16;
+	map->height = 9;
+	
+    int i = 0;
+    while (i < 9)
+	{
+	int j = 0;
+		while (map->grid[i][j])
+			printf("%c", map->grid[i][j++]);
+		printf("\n");
+		i++;
+    }
+	printf("width: %i Height: %i\n", map->width, map->height);	
 	return (SUCCESS);
 }
 
@@ -42,44 +66,12 @@ int main(int argc, char **argv)
     // init_organizer(cub, argv[1]);
     // debug(cub);
 	data_init(&data);
-	
 	printf("key down: %i\n", data.key.down);
-	char **map;
-	map = malloc(9 * sizeof(map));
-	if (map == NULL)
-		return (EXIT_FAILURE);
-	printf("malloc\n");
-    map[0] = "1111111111111111";
-    map[1] = "1000000000000001";
-    map[2] = "1001000000000111";
-    map[3] = "1000000000000001";
-    map[4] = "1000000000000001";
-    map[5] = "1000000010000001";
-    map[6] = "1000000000010001";
-    map[7] = "1000100000000001";
-    map[8] = "1111111111111111";
-	printf("map: %p\n", map);
-	// data.map.grid = map;
 	printf("player x: %f y:%f\n", data.player.pos.x, data.player.pos.y);
-	data.map.width = 16;
-	data.map.height = 9;
-	printf("player x: %f y:%f\n", data.player.pos.x, data.player.pos.y);
-	printf("key down: %i\n", data.key.down);
-    // int i = 0;
-    // while (i < 9)
-	// {
-	// int j = 0;
-	// 	while (data.map.grid[i][j])
-	// 		printf("%c", data.map.grid[i][j++]);
-	// 	printf("\n");
-	// 	i++;
-    // }
-	// printf("map w: %i H:%i\n", data.map.width, data.map.height);
-	// printf("keydown:%x\n", data.key.down);
 	
-	if (map_load(argv[1], data.map.grid) == SUCCESS && mlx_setup(&data) == SUCCESS)
+	if (map_load(argv[1], &data.map) == SUCCESS && mlx_setup(&data) == SUCCESS)
     {
-        printf("mlx_loop\n");
+		printf("mlx_loop\n");
 		event_setup(&data);
 		render(&data);
 		mlx_loop(data.mlx); 
