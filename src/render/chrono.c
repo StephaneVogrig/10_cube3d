655c36d4a40c1551f452500a33dc23761f44f857
Yohann Gaiffier <ygaiffie@3G7.42angouleme.fr>
2024-10-30 19:34:26 +0100

    add tabulation in chrono message

--- src/render/chrono.c
+++ src/render/chrono.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   chrono.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ygaiffie <ygaiffie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 22:34:54 by svogrig           #+#    #+#             */
-/*   Updated: 2024/09/17 23:02:16 by svogrig          ###   ########.fr       */
+/*   Updated: 2024/10/30 17:13:07 by ygaiffie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,5 @@ void	fps_print(clock_t delay)
 	if (delay == 0)
 		printf("fps: infini\n");
 	else
-		printf("time: %ld fps: %ld\n", delay, CLOCKS_PER_SEC / delay);
+		printf("time: %ld\tfps: %ld\n", delay, CLOCKS_PER_SEC / delay);
 }
