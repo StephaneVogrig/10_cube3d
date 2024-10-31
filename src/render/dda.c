9d490b8a0595dc26bd74932b5010374c1af858a9
Stephane Vogrig <svogrig@3G5.42angouleme.fr>
2024-10-31 16:43:38 +0100

    refactor dda_init DRY

--- src/render/dda.c
+++ src/render/dda.c
@@ -6,72 +6,37 @@
 /*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 01:53:10 by svogrig           #+#    #+#             */
-/*   Updated: 2024/10/30 23:28:28 by svogrig          ###   ########.fr       */
+/*   Updated: 2024/10/31 16:40:55 by svogrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "dda.h"
 
-void	dda_init(t_dda *raylen, t_vec2i *step, t_vec2d *raydir, t_vec2d *box)
+/*
+	compute basic data need for dda algorythme on one axis
+		- unit distance for a step, set by ptr unit
+		- length between player and box border, set by ptr side
+		- step value, set by return.
+*/
+int	dda_set(double *unit, double *side, double ray_vec, double box)
 {
-	raylen->unit.x = fabs(1 / raydir->x);
-	raylen->unit.y = fabs(1 / raydir->y);
-	if (raydir->x < 0)
+	*unit = fabs(1 / ray_vec);
+	if (ray_vec < 0)
 	{
-		step->x = -1;
-		raylen->side.x = box->x * raylen->unit.x;
-	}
-	else
-	{
-		step->x = 1;
-		raylen->side.x = (1 - box->x) * raylen->unit.x;
-	}
-	if (raydir->y < 0)
-	{
-		step->y = -1;
-		raylen->side.y = box->y * raylen->unit.y;
-	}
-	else
-	{
-		step->y = 1;
-		raylen->side.y = (1 - box->y) * raylen->unit.y;
-	}
+		*side = box * *unit;
+		return (-1);
+	}	
+	*side = (1 - box) * *unit;
+	return (1);
 }
 
-// t_ray	dda_ray(t_ray ray, t_vec2i step, t_dda raylen, t_player *player, t_vec2d *raydir)
-// {
-// 	double delta;
-// 	if (ray.hit_side == 'x')
-// 	{
-// 		if (step.x == 1)
-// 			ray.hit_side = 'w';
-// 		else
-// 			ray.hit_side = 'e';
-// 		ray.hit_pos.grid.x = ray.hit_pos.grid.x - step.x;
-// 		ray.hit_pos.box.x = 0;
-// 		delta = player->box.y + raydir->y * ray.len;
-// 		ray.hit_pos.grid.y = player->grid.y + (int)delta ;
-// 		ray.hit_pos.box.y = delta - (int)delta;
-// 		if (delta < 0)
-// 			ray.hit_pos.box.y += 1;
-// 	}
-// 	else
-// 	{
-// 		if (step.y == 1)
-// 			ray.hit_side = 'n';
-// 		else
-// 			ray.hit_side = 's';
-// 		ray.hit_pos.grid.y = ray.hit_pos.grid.y - step.y;
-// 		ray.hit_pos.box.y = 0;
-// 		delta = player->box.x + raydir->x * ray.len;
-// 		ray.hit_pos.grid.x = player->grid.x + (int)delta;
-// 		ray.hit_pos.box.x = delta - (int)delta;
-// 		if (delta < 0)
-// 			ray.hit_pos.box.x += 1;
-// 	}
-// 	return (ray);
-// }
+void	dda_init(t_dda *raylen, t_vec2i *step, t_vec2d *ray_vec, t_vec2d *box)
+{
+	step->x = dda_set(&raylen->unit.x, &raylen->side.x, ray_vec->x, box->x);
+	step->y = dda_set(&raylen->unit.y, &raylen->side.y, ray_vec->y, box->y);
+		printf("ray_vec:%f unit: %f\n", ray_vec->x, raylen->unit.x);
 
+}
 
 t_ray	dda(t_vec2d *raydir, t_map *map, t_player *player, int len_max)
 {
