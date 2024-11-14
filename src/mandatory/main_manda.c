bd620f45d6ef6af2cdc757766ef44127c5c53627
StephaneVogrig <stephane.vogrig@gmail.com>
2024-11-14 19:15:10 +0100

    fix MacroLibx leak

--- src/mandatory/main_manda.c
+++ src/mandatory/main_manda.c
@@ -1,14 +1,14 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   main_manda.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ygaiffie <ygaiffie@student.42.fr>          +#+  +:+       +#+        */
+/*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 02:16:47 by aska              #+#    #+#             */
-/*   Updated: 2024/11/13 17:50:26 by ygaiffie         ###   ########.fr       */
+/*   Updated: 2024/11/14 19:07:14 by svogrig          ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "main.h"
 #include "floor_ceil_mlx_img.h"
@@ -24,8 +24,6 @@ int	main(int argc, char **argv)
 		return (exit_code);
 	data_init(&data);
 	exit_code = data_setup(&data, argv[1]);
-	if (exit_code == SUCCESS)
-		floor_ceil_init(&data.win, data.map.textures.ceil_rgb.integer, data.map.textures.floor_rgb.integer);
 	if (exit_code == SUCCESS)
 		floor_ceil_init(&data.win, data.map.textures.ceil_rgb.integer, data.map.textures.floor_rgb.integer);
 	if (exit_code == SUCCESS)
