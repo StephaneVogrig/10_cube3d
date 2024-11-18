c8179e618efd0a8f79ab38d48d79488b06dc251f
Stephane Vogrig <svogrig@3C6.42angouleme.fr>
2024-11-18 23:29:20 +0100

    chrono: change clock by gettimeofday

--- include/chrono.h
+++ include/chrono.h
@@ -6,7 +6,7 @@
 /*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 22:32:25 by svogrig           #+#    #+#             */
-/*   Updated: 2024/09/17 23:02:48 by svogrig          ###   ########.fr       */
+/*   Updated: 2024/11/18 21:15:08 by svogrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,17 @@
 
 # include <time.h>
 # include <stdio.h>
+# include <sys/time.h>
 
 # define START 0
 # define STOP 1
 
-long	chrono(int action);
-void	fps_print(clock_t delay);
+# define USECOND_PER_SECOND 1000000
+
+typedef struct timeval t_timeval;
+typedef	unsigned long t_time;
+
+t_time	chrono(int action);
+void	fps_print(t_time delay);
 
 #endif
--- src/render/chrono.c
+++ src/render/chrono.c
@@ -3,30 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   chrono.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ygaiffie <ygaiffie@student.42.fr>          +#+  +:+       +#+        */
+/*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 22:34:54 by svogrig           #+#    #+#             */
-/*   Updated: 2024/10/30 17:13:07 by ygaiffie         ###   ########.fr       */
+/*   Updated: 2024/11/18 22:15:52 by svogrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "chrono.h"
 
-clock_t	chrono(int action)
+t_time	chrono(int action)
 {
-	static clock_t	time;
+	static t_timeval	init;
+	t_timeval			current;
+	t_time				time;
 
 	if (action == START)
-		time = clock();
-	else
-		return (clock() - time);
+	{
+		gettimeofday(&init, NULL);
+		return (0);
+	}
+	gettimeofday(&current, NULL);
+	time = (current.tv_sec - init.tv_sec) * USECOND_PER_SECOND;
+	time += current.tv_usec - init.tv_usec;
 	return (time);
+
 }
 
-void	fps_print(clock_t delay)
+void	fps_print(t_time delay)
 {
 	if (delay == 0)
 		printf("fps: infini\n");
 	else
-		printf("time: %ld\tfps: %ld\n", delay, CLOCKS_PER_SEC / delay);
+		printf("time: %ld\tfps: %ld\n", delay, USECOND_PER_SECOND / delay);
 }
