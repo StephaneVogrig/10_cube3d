9e50c2945f99f9c41940e2b35ca16ba6af89fae1
StephaneVogrig <stephane.vogrig@gmail.com>
2024-12-16 18:45:46 +0100

    delete useless code in mandatory as mouse events

--- src/mandatory/event_manda.c
+++ src/mandatory/event_manda.c
@@ -6,12 +6,11 @@
 /*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 00:47:13 by svogrig           #+#    #+#             */
-/*   Updated: 2024/12/07 15:43:12 by svogrig          ###   ########.fr       */
+/*   Updated: 2024/12/16 18:38:03 by svogrig          ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "event_manda.h"
-#include "gametime.h"
 
 int	on_win_event(int event, void *param)
 {
@@ -20,20 +19,14 @@ int	on_win_event(int event, void *param)
 	window = (t_window *)param;
 	if (event == ON_DEMAND_CLOSE)
 		mlx_loop_end(window->mlx);
-	else if (event == ON_FOCUS_GAIN)
-		window->focused = 1;
-	else if (event == ON_FOCUS_LOSS)
-		window->focused = 0;
 	return (SUCCESS);
 }
 
 int	on_keydown(int key, void *param)
 {
-	// printf("key pressed: %i\n", key);
 	t_data *data;
 	
 	data = (t_data *)param;
-
 	if (key == KEY_ESC)
 		data->key.esc = DOWN;
 	else if (key == KEY_W)
@@ -49,18 +42,6 @@ int	on_keydown(int key, void *param)
 	else if (key == KEY_RIGHT)
 		data->key.right = DOWN;
 	return (SUCCESS);
-}
-
-void	mouse_mode_switch(void *mlx, t_window *win, int *mouse_mode)
-{
-	*mouse_mode = *mouse_mode == 0;
-	if (*mouse_mode == 1)
-	{
-		mlx_mouse_move(mlx, win->win, win->width / 2, win->height / 2);
-		mlx_mouse_hide();
-	}
-	else
-		mlx_mouse_show();
 }
 
 int	on_keyup(int key, void *param)
@@ -80,8 +61,6 @@ int	on_keyup(int key, void *param)
 		data->key.left = UP;
 	else if (key == KEY_RIGHT)
 		data->key.right = UP;
-	else if (key == KEY_T && data->win.focused)
-		mouse_mode_switch(data->mlx, &data->win, &data->mouse_mode);
 	return (SUCCESS);
 }
 
@@ -91,8 +70,6 @@ int	on_loop(void *param)
 	t_time_us	delta_time;
 	t_data *data;
 	t_vec2i move;
-	int x;
-	int y;
 	int render_needed;
 
 	delta_time = gametime() - oldtime;
@@ -102,18 +79,6 @@ int	on_loop(void *param)
 	if (data->key.esc == DOWN)
 		mlx_loop_end(data->mlx);
 	render_needed = FALSE;
-	if (data->win.focused && data->mouse_mode)
-	{
-		mlx_mouse_get_pos(data->mlx, &x, &y);
-		int dx = x - data->win.width / 2;
-		if (dx)
-		{
-			data->player.dir += M_PI * dx / data->win.width;
-			// printf("mouse x:%i, y:%i\n", x, y);
-			render_needed = TRUE;
-			mlx_mouse_move(data->mlx, data->win.win, data->win.width / 2, data->win.height / 2);
-		}
-	}
 	if (data->key.down)
 	{
 		int	sign_rot = data->key.right - data->key.left;
@@ -138,34 +103,10 @@ int	on_loop(void *param)
 	return (SUCCESS);
 }
 
-int on_mousedown(int button, void *param)
-{
-	(void)button;
-	(void)param;
-	t_data *data;
-
-	data = (t_data *)param;
-	(void)data;
-	return(SUCCESS);
-}
-
-int on_mouseup(int button, void *param)
-{
-	(void)button;
-	(void)param;
-	t_data *data;
-
-	data = (t_data *)param;
-	(void)data;
-	return(SUCCESS);
-}
-
 void	event_setup(t_data *data)
 {
 	mlx_on_event(data->mlx, data->win.win, MLX_WINDOW_EVENT, on_win_event, &data->win);
 	mlx_on_event(data->mlx, data->win.win, MLX_KEYDOWN, on_keydown, data);
 	mlx_on_event(data->mlx, data->win.win, MLX_KEYUP, on_keyup, data);
-	mlx_on_event(data->mlx, data->win.win, MLX_MOUSEDOWN, on_mousedown, data);
-	mlx_on_event(data->mlx, data->win.win, MLX_MOUSEUP, on_mouseup, data);
 	mlx_loop_hook(data->mlx, on_loop, data);
 }
