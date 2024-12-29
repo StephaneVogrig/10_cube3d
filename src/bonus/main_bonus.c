92c7680c73fdbc30d11b3df7adf3675de743dcd5
Aska <106731287+Askandrie@users.noreply.github.com>
2024-12-29 17:09:16 +0100

    map vleaning

--- src/bonus/main_bonus.c
+++ src/bonus/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: aska <aska@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 20:37:20 by svogrig           #+#    #+#             */
-/*   Updated: 2024/12/28 23:21:51 by aska             ###   ########.fr       */
+/*   Updated: 2024/12/29 00:41:42 by aska             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int program_init(t_data *data, t_option *option, int argc, char **argv)
 		return (exit_code);
 	ext = ft_strrchr(argv[1], '.');
 	if (ext == NULL || ft_strcmp(ext, ".cub") != 0)
-		return (ft_return(ERROR, 3, "L.23:check_entry_arg: No \".cub\" extension map file"));
+		return (ft_return(ERROR, 3, "L.26:program_init: No \".cub\" extension map file"));
 	data->win.width = option->win_width;
 	data->win.height = option->win_height;
 	return (exit_code);
--- src/bonus/option_bonus.c
+++ src/bonus/option_bonus.c
@@ -6,18 +6,31 @@
 /*   By: aska <aska@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 01:30:59 by aska              #+#    #+#             */
-/*   Updated: 2024/12/28 22:17:11 by aska             ###   ########.fr       */
+/*   Updated: 2024/12/29 01:14:21 by aska             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "option_bonus.h"
 
-void    option_init_default(t_option *option)
+static void    option_init_default(t_option *option)
 {
     option->win_width = WIN_W;
     option->win_height = WIN_H;
     option->fov = 1;
     option->minimap = FALSE;
+}
+
+static int option_parser(char *key, char *value, t_option *option)
+{
+    if (ft_strcmp(key, "--resolution") == 0)
+        return (option_get_resolution(option, value));
+    else if (ft_strcmp(key, "--fov") == 0 && is_valid_color(value))
+        option->fov = ft_atoi(value);
+    else if (ft_strcmp(key, "--minimap") == 0)
+        option->minimap = TRUE;
+    else
+        return (FAIL);
+    return (SUCCESS);
 }
 
 int    option_init(t_option *option, int ac, char **av)
@@ -27,6 +40,8 @@ int    option_init(t_option *option, int ac, char **av)
     t_key_value argument;
 
     if (ac < 2)
+        ft_display(ERROR, "No map file");
+    if (ac < 2)
         return (print_help());
     i = 2;
     exit_code = 0;
@@ -34,15 +49,12 @@ int    option_init(t_option *option, int ac, char **av)
     while (i < ac)
     {
         exit_code = option_tokenizer(&argument.key, &argument.value, av[i++]);
-        if (exit_code == SUCCESS && ft_strcmp(argument.key, "--resolution") == 0)
-            option_get_resolution(option, argument.value);
-        else if (exit_code == SUCCESS && ft_strcmp(argument.key, "--fov") == 0 && is_valid_color(argument.value))
-            option->fov = ft_atoi(argument.value);
-        else if (ft_strcmp(argument.key, "--minimap") == 0)
-            option->minimap = TRUE;
-        else
+        if (exit_code == FAIL)
             return (print_help());
-    }
+        exit_code = option_parser(argument.key, argument.value, option);
+        if (exit_code == FAIL)
+            return (print_help());
+    }   
     return (exit_code);
 }
 
