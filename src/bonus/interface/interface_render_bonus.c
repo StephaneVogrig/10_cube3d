b0e3ad54b43155be8db269ef1806f6485fe784d9
Aska <106731287+Askandrie@users.noreply.github.com>
2025-01-21 02:07:38 +0100

    finish map display

--- src/bonus/interface/interface_render_bonus.c
+++ src/bonus/interface/interface_render_bonus.c
@@ -6,94 +6,54 @@
 /*   By: aska <aska@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/28 15:03:52 by svogrig           #+#    #+#             */
-/*   Updated: 2025/01/19 18:41:20 by aska             ###   ########.fr       */
+/*   Updated: 2025/01/20 17:16:07 by aska             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "interface_render_bonus.h"
 
-void	draw_player(t_interface *interface, t_player *player)
+void	draw_player(t_interface *interface)
 {
 	t_vec2i	start;
 	t_vec2i	end;
 
-	printf("interface->scale: %d\n", interface->scale);
-	start.x = interface->scale * gridbox_to_double(player->x);
-	start.y = interface->scale * gridbox_to_double(player->y);
-	end.x = start.x + 2;
-	end.y = start.y + 2;
-	draw_rectangle(interface, start, end, 0xFFFFFF00);
+	start.x = MINIMAP_W / 2;
+	start.y = MINIMAP_H / 2;
+	end.x = start.x + 23;
+	end.y = start.y + 23;
+	draw_rectangle(interface, start, end, 0xAAFFFF00);
 }
 
-// void	draw_interface(t_interface *interface, t_map *map)
-// {
-// 	int		x;
-// 	int		y;
-// 	int		color;
-// 	t_vec2i	begin;
-// 	t_vec2i	end;
+void	draw_rays(t_interface *interface, t_ray_tab *rays)
+{
+	t_vec2i	start;
+	t_vec2i	end;
+	t_ray	*ray;
+	int		i;
 
-// 	begin.y = 1;
-// 	end.y = interface->scale - 1;
-// 	y = 0;
-// 	while (y < map->height)
-// 	{
-// 		x = 0;
-// 		begin.x = 1;
-// 		end.x = interface->scale - 1;
-// 		while (x < map->width)
-// 		{
-// 			if (map->grid[y][x] != 48)
-// 				color = 0xFF0FFFfF;
-// 			else
-// 				color = 0xFF7F7F7F;
-// 			// draw_rectangle(interface, begin, end, color);
-// 			// draw_line(interface, vector2i(begin.x - 1, begin.y - 1),
-// 				vector2i(begin.x - 1, begin.y - 1 + interface->scale),
-// 				0xFF606060);
-// 			// draw_line(interface, vector2i(begin.x - 1, begin.y - 1),
-// 				vector2i(begin.x - 1 + interface->scale, begin.y - 1),
-// 				0xFF606060);
-// 			begin.x += interface->scale;
-// 			end.x += interface->scale;
-// 			x++;
-// 		}
-// 		begin.y += interface->scale;
-// 		end.y += interface->scale;
-// 		y++;
-// 	}
-// }
-
-// void	draw_rays(t_interface *interface, t_player *player, t_ray_tab *rays)
-// {
-// 	// t_vec2i	start;
-// 	// t_vec2i	end;
-// 	// t_ray	*ray;
-// 	// int		i;
-
-// 	// start.x = interface->scale * gridbox_to_double(player->x);
-// 	// start.y = interface->scale * gridbox_to_double(player->y);
-// 	// i = 0;
-// 	// ray = rays->tab;
-// 	// while (i++ < rays->nbr)
-// 	// {
-// 	// 	end.x = interface->scale * gridbox_to_double(ray->hit_pos.x);
-// 	// 	end.y = interface->scale * gridbox_to_double(ray->hit_pos.y);
-// 	// 	// draw_line(interface, start, end, 0xFFFF0000);
-// 	// 	ray++;
-// 	// }
-// }
+	start.x = (MINIMAP_W >> 1) + (TILES_W >> 1);
+	start.y = (MINIMAP_H >> 1) + (TILES_H >> 1);
+	i = 0;
+	ray = rays->tab;
+	while (i++ < rays->nbr)
+	{
+		end.x = (MINIMAP_W >> 1) * gridbox_to_double(ray->hit_pos.x);
+		end.y = (MINIMAP_H >> 1) * gridbox_to_double(ray->hit_pos.y);
+		draw_line(interface, start, end, 0x56AABB00);
+		ray++;
+	}
+}
 
 void	render_interface(t_interface *interface, t_data *data)
 {
-	(void)data;
+	int x_minimap;
+	int y_minimap;
+
+	x_minimap = (MINIMAP_W >> 1) - data->player.x.grid * TILES_W;
+	y_minimap = (MINIMAP_H >> 1) - data->player.y.grid * TILES_H;
 	mlx_put_image_to_window(interface->mlx, interface->win, interface->img_bg, 0, 0);
-	mlx_put_image_to_window(interface->mlx, interface->win, interface->img_map, 72, 96);
+	mlx_put_image_to_window(interface->mlx, interface->win, interface->img_map, x_minimap, y_minimap);
 	mlx_put_image_to_window(interface->mlx, interface->win, interface->img_fg, 0, 0);
-	// draw_interface(interface, &data->map);
-	// draw_rays(interface, &data->player, &data->rays);
-	printf("player x: %d\n", data->player.x.grid);
-	printf("player y: %d\n", data->player.y.grid);
-	
-	draw_player(interface, &data->player);
+	// draw_rays(interface, &data->rays);
+	draw_player(interface);
 }
