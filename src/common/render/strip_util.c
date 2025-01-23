2481d3ea44c5d199bdf995b7b09d8b5c16201ef5
Stephane Vogrig <svogrig@2D2.42angouleme.fr>
2025-01-23 18:50:31 +0100

    fix junction wall floor ceil

--- src/common/render/strip_util.c
+++ src/common/render/strip_util.c
@@ -6,20 +6,29 @@
 /*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 18:48:25 by svogrig           #+#    #+#             */
-/*   Updated: 2025/01/20 15:17:41 by svogrig          ###   ########.fr       */
+/*   Updated: 2025/01/23 18:48:54 by svogrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "limits.h"
+#include <stdio.h>
 
 int	strip_screen_size(int screen_scale, double distance)
 {
-	float size;
+	float	size_f;
+	int		size_i;
 
 	if (distance == 0)
 		return (INT_MAX);
-	size = screen_scale / distance;
-	if (size > (float)INT_MAX)
+	size_f = screen_scale / distance;
+	if (size_f < 1.5)
+		return (0);
+	if (size_f > (float)INT_MAX)
 		return (INT_MAX);
-	return ((int)size);
+	size_i = (int)(size_f + 0.5);
+	if (size_i % 2 == 0)
+		size_i += 2;
+	else
+		size_i++;
+	return (size_i);
 }
