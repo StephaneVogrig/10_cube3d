529b4fe634aa10ec86c8c5e7d330508c08d47f39
Aska <106731287+Askandrie@users.noreply.github.com>
2025-01-30 12:18:57 +0100

    fix color_set_rgb

--- src/common/color.c
+++ src/common/color.c
@@ -6,7 +6,7 @@
 /*   By: aska <aska@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 02:53:38 by svogrig           #+#    #+#             */
-/*   Updated: 2025/01/30 03:18:11 by aska             ###   ########.fr       */
+/*   Updated: 2025/01/30 12:18:35 by aska             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,6 @@ static int	char_to_rgb(char **value, unsigned char *rgb)
 	int c;
 	
 	c = cub_strtoi(*value, value);
-	ft_printf("c = %d\n", c);
 	if (c < 0 || c > 255)
 		return (FAIL);
 	*rgb = (unsigned char)c;
@@ -54,16 +53,14 @@ int	color_set_rgb(t_rgb *rgb, char *value)
 	tmp_value = value;
 
 	exit_code = char_to_rgb(&value, &rgb->r);
-	ft_printf("c on return = %d\n", rgb->integer);
 	if (exit_code == FAIL || *value == '\0')
 		return (ft_return(ERROR, FAIL, "Invalid color", tmp_value));
 	exit_code = char_to_rgb(&value, &rgb->g);
-	ft_printf("c on return = %d\n", rgb->integer);
 	if (exit_code == FAIL || *value == '\0')
 		return (ft_return(ERROR, FAIL, "Invalid color", tmp_value));
 	exit_code = char_to_rgb(&value, &rgb->b);
-	ft_printf("c on return = %d\n", rgb->integer);
 	if (exit_code == FAIL)
 		return (ft_return(ERROR, FAIL, "Invalid color", tmp_value));
+	rgb->a = 255;
 	return (SUCCESS);
 }
